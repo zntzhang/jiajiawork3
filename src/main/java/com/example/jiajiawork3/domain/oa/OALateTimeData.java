b575f64c061184f8ce8056dba7e2f123c4ec172f
zhangtao <chuiri.zt@raycloud.com>
2022-11-29 11:26:51 +0800

    迟到

---
+++ src/main/java/com/example/jiajiawork3/domain/oa/OALateTimeData.java
@@ -0,0 +1,15 @@
+package com.example.jiajiawork3.domain.oa;
+
+import lombok.Data;
+
+/**
+ * @program: jiajiawork3
+ * @description:
+ * @author: chunri
+ * @create: 2022-11-29 11:09
+ **/
+@Data
+public
+class OALateTimeData {
+    private String result;
+}
---
+++ src/main/java/com/example/jiajiawork3/domain/oa/OALateTimeDataResult.java
@@ -0,0 +1,16 @@
+package com.example.jiajiawork3.domain.oa;
+
+import lombok.Data;
+
+/**
+ * @program: jiajiawork3
+ * @description:
+ * @author: chunri
+ * @create: 2022-11-29 11:09
+ **/
+@Data
+public class OALateTimeDataResult {
+    private String timeStr;
+    private Integer lateTime;
+}
+
